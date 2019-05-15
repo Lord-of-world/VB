001442d12b1ebcfacfcacf11e206bf7cec367a15
Эдгар Адамян <edgar1999@bk.ru>
2019-05-15 18:34:20 +0300

    =

---
+++ РОМБ ДВИГАЕТСЯ ПО КОНТУРУ/1305/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.РомбикToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ОчиститьToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ВыходToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PB = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PB, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.РомбикToolStripMenuItem, Me.ОчиститьToolStripMenuItem, Me.ВыходToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(707, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'РомбикToolStripMenuItem
+        '
+        Me.РомбикToolStripMenuItem.Name = "РомбикToolStripMenuItem"
+        Me.РомбикToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
+        Me.РомбикToolStripMenuItem.Text = "Ромбик"
+        '
+        'ОчиститьToolStripMenuItem
+        '
+        Me.ОчиститьToolStripMenuItem.Name = "ОчиститьToolStripMenuItem"
+        Me.ОчиститьToolStripMenuItem.Size = New System.Drawing.Size(71, 20)
+        Me.ОчиститьToolStripMenuItem.Text = "Очистить"
+        '
+        'ВыходToolStripMenuItem
+        '
+        Me.ВыходToolStripMenuItem.Name = "ВыходToolStripMenuItem"
+        Me.ВыходToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.ВыходToolStripMenuItem.Text = "Выход"
+        '
+        'PB
+        '
+        Me.PB.Location = New System.Drawing.Point(12, 27)
+        Me.PB.Name = "PB"
+        Me.PB.Size = New System.Drawing.Size(683, 459)
+        Me.PB.TabIndex = 1
+        Me.PB.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 50
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 50
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 50
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(707, 498)
+        Me.Controls.Add(Me.PB)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PB, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents РомбикToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ОчиститьToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ВыходToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PB As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+
+End Class
---
+++ РОМБ ДВИГАЕТСЯ ПО КОНТУРУ/1305/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Dim g As Graphics, n, i As Integer
+    Dim T() As PointF = {New PointF(0, 40), New PointF(40, 0), New PointF(80, 40), New PointF(40, 80)}
+
+    Private Sub РомбикToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles РомбикToolStripMenuItem.Click
+        g = PB.CreateGraphics
+        n = UBound(T)
+        Ромб()
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub ОчиститьToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ОчиститьToolStripMenuItem.Click
+        PB.Refresh()
+    End Sub
+
+    Private Sub ВыходToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ВыходToolStripMenuItem.Click
+        End
+    End Sub
+    Private Sub Ромб()
+        Dim k As New SolidBrush(Color.BlueViolet)
+        Dim p As New Pen(Color.Black, 2)
+        g.FillPolygon(k, T)
+        g.DrawPolygon(p, T)
+    End Sub
+
+    Private Sub Движение1()
+        For i = 0 To n
+            T(i).X = T(i).X + 5
+        Next
+    End Sub
+
+    Private Sub Движение2()
+        For i = 0 To n
+            T(i).Y = T(i).Y + 5
+        Next
+    End Sub
+
+    Private Sub Движение3()
+        For i = 0 To n
+            T(i).X = T(i).X - 5
+        Next
+    End Sub
+
+    Private Sub Движение4()
+        For i = 0 To n
+            T(i).Y = T(i).Y - 5
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        PB.Refresh()
+        Движение1()
+        Ромб()
+        If T(2).X > PB.Width - 10 Then Timer1.Enabled = False : Timer2.Enabled = True
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        PB.Refresh()
+        Движение2()
+        Ромб()
+        If T(3).Y > PB.Height - 10 Then Timer2.Enabled = False : Timer3.Enabled = True
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        PB.Refresh()
+        Движение3()
+        Ромб()
+        If T(0).X = 0 Then Timer3.Enabled = False : Timer4.Enabled = True
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        PB.Refresh()
+        Движение4()
+        Ромб()
+        If T(1).Y = 0 Then Timer4.Enabled = False : Timer1.Enabled = True
+    End Sub
+End Class
