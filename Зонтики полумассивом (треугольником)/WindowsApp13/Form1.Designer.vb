054fc29348bee2c1fe9c86bf2420ec6843a05633
Эдгар Адамян <edgar1999@bk.ru>
2019-05-22 20:20:59 +0300

    "

--- Зонтики полумассивом (треугольником)/WindowsApp13/Form1.Designer.vb
+++ Зонтики полумассивом (треугольником)/WindowsApp13/Form1.Designer.vb
@@ -22,9 +22,11 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.КлумбаToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ОчиститьToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.МассивЗонтиковToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ВвввввввввввввваааааааааToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ВыходToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PB = New System.Windows.Forms.PictureBox()
         Me.MenuStrip1.SuspendLayout()
@@ -34,24 +36,30 @@ Partial Class Form1
         'MenuStrip1
         '
         Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.КлумбаToolStripMenuItem, Me.ОчиститьToolStripMenuItem, Me.ВыходToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.МассивЗонтиковToolStripMenuItem, Me.ВвввввввввввввваааааааааToolStripMenuItem, Me.ВыходToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1424, 33)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1211, 33)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'КлумбаToolStripMenuItem
+        'ContextMenuStrip1
         '
-        Me.КлумбаToolStripMenuItem.Name = "КлумбаToolStripMenuItem"
-        Me.КлумбаToolStripMenuItem.Size = New System.Drawing.Size(84, 29)
-        Me.КлумбаToolStripMenuItem.Text = "Клумба"
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
         '
-        'ОчиститьToolStripMenuItem
+        'МассивЗонтиковToolStripMenuItem
         '
-        Me.ОчиститьToolStripMenuItem.Name = "ОчиститьToolStripMenuItem"
-        Me.ОчиститьToolStripMenuItem.Size = New System.Drawing.Size(99, 29)
-        Me.ОчиститьToolStripMenuItem.Text = "Очистить"
+        Me.МассивЗонтиковToolStripMenuItem.Name = "МассивЗонтиковToolStripMenuItem"
+        Me.МассивЗонтиковToolStripMenuItem.Size = New System.Drawing.Size(166, 29)
+        Me.МассивЗонтиковToolStripMenuItem.Text = "Массив зонтиков"
+        '
+        'ВвввввввввввввваааааааааToolStripMenuItem
+        '
+        Me.ВвввввввввввввваааааааааToolStripMenuItem.Name = "ВвввввввввввввваааааааааToolStripMenuItem"
+        Me.ВвввввввввввввваааааааааToolStripMenuItem.Size = New System.Drawing.Size(99, 29)
+        Me.ВвввввввввввввваааааааааToolStripMenuItem.Text = "Очистить"
         '
         'ВыходToolStripMenuItem
         '
@@ -63,20 +71,20 @@ Partial Class Form1
         '
         Me.PB.Location = New System.Drawing.Point(12, 36)
         Me.PB.Name = "PB"
-        Me.PB.Size = New System.Drawing.Size(1400, 664)
-        Me.PB.TabIndex = 1
+        Me.PB.Size = New System.Drawing.Size(1187, 608)
+        Me.PB.TabIndex = 2
         Me.PB.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1424, 697)
+        Me.ClientSize = New System.Drawing.Size(1211, 656)
         Me.Controls.Add(Me.PB)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
-        Me.Text = "Клумба"
+        Me.Text = "Form1"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         CType(Me.PB, System.ComponentModel.ISupportInitialize).EndInit()
@@ -86,8 +94,9 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents MenuStrip1 As MenuStrip
-    Friend WithEvents PB As PictureBox
-    Friend WithEvents КлумбаToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ОчиститьToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents МассивЗонтиковToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ВвввввввввввввваааааааааToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ВыходToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PB As PictureBox
 End Class
---
+++ Зонтики полумассивом (треугольником)/WindowsApp13/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim g As Graphics
+    Dim x, y, i, j, n, h As Single
+    Private Sub МассивЗонтиковToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles МассивЗонтиковToolStripMenuItem.Click
+        PB.Refresh()
+        Do
+            n = Val(InputBox("Введите количество зонтиков в массиве"))
+        Loop Until n > 0
+        h = PB.ClientSize.Height \ n
+        y = 0
+        For i = 1 To n
+            x = 0
+            For j = 1 To n
+                If i >= j Then Зонтик()
+                x = x + h
+            Next
+            y = y + h
+        Next
+    End Sub
+
+    Private Sub ОчиститьToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ВвввввввввввввваааааааааToolStripMenuItem.Click
+        PB.Refresh()
+    End Sub
+
+    Private Sub ВыходToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ВыходToolStripMenuItem.Click
+        End
+    End Sub
+    Public Sub Зонтик()
+        Dim k As New SolidBrush(Color.Yellow)
+        Dim p As New Pen(Color.Orange, 2)
+        Dim p1 As New Pen(Color.Green, 3)
+        g = PB.CreateGraphics
+        g.FillPie(k, x, y, h - 10, h, 180, 180)
+        g.DrawPie(p, x, y, h - 10, h, 180, 180)
+        g.DrawLine(p1, x + (h - 10) \ 2, y + h \ 2, x + (h - 10) \ 2, y + h - h \ 5)
+        g.DrawArc(p1, x + (h - 10) \ 2 - h \ 5, y + h - h \ 5 - h \ 9, h \ 5, h \ 5, 0, 180)
+    End Sub
+End Class
