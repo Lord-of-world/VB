66cdac20ad7c5509a62369781ae212380a8ab480
Эдгар Адамян <edgar1999@bk.ru>
2019-05-22 19:33:03 +0300

    '

---
+++ клумба/клумба/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.PB = New System.Windows.Forms.PictureBox()
+        Me.КлумбаToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ОчиститьToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ВыходToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PB, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.КлумбаToolStripMenuItem, Me.ОчиститьToolStripMenuItem, Me.ВыходToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1424, 33)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'PB
+        '
+        Me.PB.Location = New System.Drawing.Point(12, 36)
+        Me.PB.Name = "PB"
+        Me.PB.Size = New System.Drawing.Size(1400, 664)
+        Me.PB.TabIndex = 1
+        Me.PB.TabStop = False
+        '
+        'КлумбаToolStripMenuItem
+        '
+        Me.КлумбаToolStripMenuItem.Name = "КлумбаToolStripMenuItem"
+        Me.КлумбаToolStripMenuItem.Size = New System.Drawing.Size(84, 29)
+        Me.КлумбаToolStripMenuItem.Text = "Клумба"
+        '
+        'ОчиститьToolStripMenuItem
+        '
+        Me.ОчиститьToolStripMenuItem.Name = "ОчиститьToolStripMenuItem"
+        Me.ОчиститьToolStripMenuItem.Size = New System.Drawing.Size(99, 29)
+        Me.ОчиститьToolStripMenuItem.Text = "Очистить"
+        '
+        'ВыходToolStripMenuItem
+        '
+        Me.ВыходToolStripMenuItem.Name = "ВыходToolStripMenuItem"
+        Me.ВыходToolStripMenuItem.Size = New System.Drawing.Size(76, 29)
+        Me.ВыходToolStripMenuItem.Text = "Выход"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1424, 697)
+        Me.Controls.Add(Me.PB)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PB, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents PB As PictureBox
+    Friend WithEvents КлумбаToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ОчиститьToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ВыходToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ клумба/клумба/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Dim g As Graphics
+    Dim x, xc, y, yc, i, n, grad, r, h, rad As Single
+
+    Private Sub КлумбаToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles КлумбаToolStripMenuItem.Click
+        PB.Refresh()
+        Do
+            n = Val(InputBox("Введите количество цветков в клумбе"))
+        Loop Until n > 0
+        x = 0 : y = 0
+        grad = 2 * Math.PI / n
+        xc = PB.ClientSize.Width \ 2
+        yc = PB.ClientSize.Height \ 2
+        h = PB.ClientSize.Height \ 8
+        rad = 0
+        r = PB.ClientSize.Height \ 3
+        For i = 1 To n
+            x = xc + r * Math.Cos(rad)
+            y = yc + r * Math.Sin(rad)
+            Цветок()
+            rad = rad + grad
+        Next
+    End Sub
+
+    Private Sub ОчиститьToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ОчиститьToolStripMenuItem.Click
+        PB.Refresh()
+    End Sub
+
+    Private Sub ВыходToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ВыходToolStripMenuItem.Click
+        End
+    End Sub
+
+    Public Sub Цветок()
+        Dim k As New SolidBrush(Color.Yellow)
+        Dim p As New Pen(Color.Orange, 1)
+        Dim p1 As New Pen(Color.Green, 1)
+        g = PB.CreateGraphics
+        g.DrawLine(p1, x, y + h \ 2, x, y + 100)
+        g.FillEllipse(k, x - h, y - h, h, h)
+        g.DrawEllipse(p, x - h, y - h, h, h)
+        g.FillEllipse(k, x, y - h, h, h)
+        g.DrawEllipse(p, x, y - h, h, h)
+        g.FillEllipse(k, x, y, h, h)
+        g.DrawEllipse(p, x, y, h, h)
+        g.FillEllipse(k, x - h, y, h, h)
+        g.DrawEllipse(p, x - h, y, h, h)
+        g.DrawLine(p1, x, y + h \ 2, x, y + 100)
+    End Sub
+End Class
