ec0a11413a0cc429e28e3cb9eb62b53c24d77e5d
Эдгар Адамян <edgar1999@bk.ru>
2019-06-24 13:11:41 +0300

    -

---
+++ РК4 (Графика)/КД4/КД4/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Dim g As Graphics, i As Integer
+    Dim T() As PointF = {New PointF(0, 20),
+        New PointF(20, 0),
+        New PointF(40, 0),
+        New PointF(60, 20),
+        New PointF(60, 40),
+        New PointF(40, 60),
+        New PointF(20, 60),
+        New PointF(0, 40)}
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        g = Me.CreateGraphics
+        Восьмиугольник()
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Восьмиугольник()
+        Dim p As New Pen(Color.Black)
+        g.DrawPolygon(p, T)
+    End Sub
+
+    Private Sub Движение1()
+        For i = 0 To 7
+            T(i).X = T(i).X + 5
+        Next
+    End Sub
+
+    Private Sub Движение2()
+        For i = 0 To 7
+            T(i).Y = T(i).Y + 5
+        Next
+    End Sub
+
+    Private Sub Движение3()
+        For i = 0 To 7
+            T(i).X = T(i).X - 5
+        Next
+    End Sub
+
+    Private Sub Движение4()
+        For i = 0 To 7
+            T(i).Y = T(i).Y - 5
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Refresh()
+        Движение1()
+        Восьмиугольник()
+        If T(3).X > Me.Width - 20 Then Timer1.Enabled = False : Timer2.Enabled = True
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.Refresh()
+        Движение2()
+        Восьмиугольник()
+        If T(5).Y > Me.Height - 45 Then Timer2.Enabled = False : Timer3.Enabled = True
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Me.Refresh()
+        Движение3()
+        Восьмиугольник()
+        If T(0).X = 0 Then Timer3.Enabled = False : Timer4.Enabled = True
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        Me.Refresh()
+        Движение4()
+        Восьмиугольник()
+        If T(1).Y = 0 Then Timer4.Enabled = False : Timer1.Enabled = True
+    End Sub
+End Class
---
+++ РК4 (Графика)/КД4/КД4/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("КД4")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("КД4")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("3490cc9e-2750-4322-a003-df2c31b02df9")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
